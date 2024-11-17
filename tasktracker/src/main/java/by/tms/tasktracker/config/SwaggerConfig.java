e0c72bfef2911fc3ac8e631a8a95ace2bff1f0ae
Dmitry <dkonchik@gmail.com>
2024-11-17 14:48:56 +0300

    SwaggerConfig

---
+++ tasktracker/src/main/java/by/tms/tasktracker/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package by.tms.tasktracker.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI api() {
+
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("https://localhost:8080")
+                        )
+                ).info(
+                        new Info().title("TaskTracker").version("1.0")
+                );
+    }
+}
+
+
