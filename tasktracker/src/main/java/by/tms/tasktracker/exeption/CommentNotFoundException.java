965445be3ef2d8515bc6ff700c81e7e5c099b16f
Dmitry <dkonchik@gmail.com>
2024-11-17 14:47:43 +0300

    Exception commit

---
+++ tasktracker/src/main/java/by/tms/tasktracker/exeption/CommentNotFoundException.java
@@ -0,0 +1,19 @@
+package by.tms.tasktracker.exeption;
+
+public class CommentNotFoundException extends RuntimeException{
+    public CommentNotFoundException() {
+        super();
+    }
+
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+
+    public CommentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
